0b2c0b9d28e4a73e57bc9cc4c997f26cf2386285
Bruno <bruno.melo@sasi.com.br>
2026-04-22 11:28:38 -0400

    Clone Saad

---
+++ nvim/.config/nvim/lua/saad/plugins/telescope.lua
@@ -0,0 +1,53 @@
+return {
+	"nvim-telescope/telescope.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		"nvim-tree/nvim-web-devicons",
+		"folke/todo-comments.nvim",
+	},
+	config = function()
+		local telescope = require("telescope")
+		local actions = require("telescope.actions")
+
+		telescope.setup({
+			defaults = {
+				path_display = { "smart" },
+				mappings = {
+					i = {
+						["<C-k>"] = actions.move_selection_previous,
+						["<C-j>"] = actions.move_selection_next,
+						["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+					},
+				},
+			},
+			pickers = {
+				find_files = {
+					file_ignore_patterns = { ".git", ".venv" },
+					hidden = true,
+					no_ignore = true,
+				},
+				live_grep = {
+					file_ignore_patterns = { ".git", ".venv" },
+					additional_args = function(_)
+						return { "--hidden" }
+					end,
+				},
+			},
+		})
+
+		pcall(telescope.load_extension, "fzf")
+		pcall(telescope.load_extension, "noice")
+		-- set keymaps
+		local keymap = vim.keymap -- for conciseness
+
+		keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+		keymap.set("n", "<leader>fr", "<cmd>Telescope registers<cr>", { desc = "Fuzzy find registers" })
+		keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
+		keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
+		keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
+		keymap.set("n", "<leader>fk", "<cmd>Telescope keymaps<cr>", { desc = "[F]ind [K]eymaps" })
+		keymap.set("n", "<leader>fd", "<cmd>Telescope diagnostics<cr>", { desc = "[F]ind [D]iagnostics" })
+		keymap.set("n", "<leader><leader>", "<cmd>Telescope buffers<cr>", { desc = "find existing buffers" })
+	end,
+}
---
+++ nvim/.config/nvim/lua/saad/plugins/treesitter.lua
@@ -0,0 +1,78 @@
+return {
+	"nvim-treesitter/nvim-treesitter",
+	event = { "BufReadPre", "BufNewFile" },
+	build = ":TSUpdate",
+	dependencies = {
+		{
+			"windwp/nvim-ts-autotag",
+			opts = {
+				opts = {
+					enable_close = true,
+					enable_rename = true,
+					enable_close_on_slash = false,
+				},
+			},
+		},
+	},
+	config = function()
+		-- import nvim-treesitter plugin
+		local treesitter = require("nvim-treesitter.configs")
+
+		require("nvim-treesitter.parsers").filetype_to_parsername.htmldjango = "html"
+
+		-- configure treesitter
+		treesitter.setup({ -- enable syntax highlighting
+			highlight = {
+				enable = true,
+				-- Remover jinja2 daqui - não existe parser jinja2 no treesitter
+				additional_vim_regex_highlighting = { "terraform" },
+				disable = { "tmux" },
+			},
+			-- enable indentation
+			indent = { enable = true },
+			-- ensure these language parsers are installed
+			ensure_installed = {
+				"json",
+				"javascript",
+				"typescript",
+				"tsx",
+				"yaml",
+				"html",
+				"css",
+				"prisma",
+				"markdown",
+				"markdown_inline",
+				"svelte",
+				"graphql",
+				"hcl",
+				"helm",
+				"toml",
+				"regex",
+				"bash",
+				"lua",
+				"python",
+				"vim",
+				"dockerfile",
+				"gitignore",
+				"query",
+				"terraform",
+				"vimdoc",
+				"c",
+				"tmux",
+			},
+			auto_install = true,
+			sync_install = false,
+			ignore_install = {},
+			modules = {},
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<C-space>",
+					node_incremental = "<C-space>",
+					scope_incremental = false,
+					node_decremental = "<bs>",
+				},
+			},
+		})
+	end,
+}
